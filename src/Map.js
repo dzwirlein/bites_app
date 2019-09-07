7c80019de662bc777c80e27d2518496496a2af78
unknown <hannahuseva@gmail.com>
2019-09-07 12:50:14 -0500

    Basic Map component

---
+++ src/Map.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// This is a try-out map Component running on free API. It doesn't allow much functionality and will be eventually replaced by more advanced paid API
+
+let search = "Casablanca, Milwaukee"
+
+function Map() {
+  const src="https://www.google.com/maps/embed/v1/place?q="+ search + "&key="+ process.env.REACT_APP_API_KEY
+  return (
+    <div>
+      <iframe width="600" height="450" frameBorder="0" stylename="border:0"
+      src={src} allowFullScreen title="map"></iframe>
+    </div>
+  );
+}
+  
+export default Map;
