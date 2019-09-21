7953851c133822dfbcffe410a535afedd4d26ece
unknown <hannahuseva@gmail.com>
2019-09-21 14:14:31 -0500

    fixed master

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css";
 
 export function Container({ fluid, children }) {
   return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
