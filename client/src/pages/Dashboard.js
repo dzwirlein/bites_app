24a70dde33078123c17f889876de7838082a9ee9
dzwirlein <47755855+dzwirlein@users.noreply.github.com>
2019-09-21 23:09:22 -0500

    Added Dashboard Page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -36,70 +36,224 @@ class Dashboard extends Component {
       })
      .catch(err => console.log(err));
     };
-    
+
+
+    lovePlace = placeData => {
+      console.log(this.state)
+      API.lovePlace(this.state.userID, placeData)
+      .then(res => {
+        this.setState({        
+          lovedplaces: res.data.swipedright
+        })
+      })
+      .catch(err => console.log(err));
+    };
+  
+    hatePlace = placeData => {
+      API.hatePlace(this.state.userID, placeData)
+      .then(res => {
+        this.setState({
+          hatedplaces: res.data.swipedleft
+        })
+      })
+      .catch(err => console.log(err));
+    };
+  
+    handleHatedCommentSubmit = event => {
+      event.preventDefault();
+      const id = event.target.id
+      API.postHatedComment(id, this.state.comment)
+      .then(res => {
+        console.log(res)
+        this.setState({     
+          comment: ""
+        })
+      })
+      .catch(err => console.log(err));
+    }
+  
+    handleLovedCommentSubmit = event => {
+      const id = event.target.id
+      event.preventDefault();
+      API.postLovedComment(id, this.state.comment)
+      .then(res => {
+        this.setState({        
+          comment: ""
+        })
+      })
+      .catch(err => console.log(err));
+    }
    
 
-render() {
-    console.log("dash state: ", this.state)
-  return (
+  
+    render() {
+      return (
 
-        <div >
-              <h1>Content for signed in users will go here</h1>
-              <MapG />
-              <Container fluid>
-              <Row>
-                <Col size="md-12">
+        <div>
+            <div>
+                <h1>Content for signed in users will go here</h1>
+                <MapG />
+                <Container fluid>
+                <Row>
+                  <Col size="md-12">
+                    <Card>
+                      <h1>Search Places</h1>
+                    </Card>
+                    {/* <Alert style={{ opacity: this.state.alert ? 1 : 0 }} type="success" >
+                    Book Saved
+                    <DeleteBtn onClick={() => this.setState({alert: false})} />
+                    </Alert> */}
+                    <form>
+                      <Input
+                        value={this.state.search}
+                        onChange={this.handleInputChange}
+                        name="search"
+                        placeholder="Search for"
+                      />
+                      <FormBtn disabled={!(this.state.search)}onClick={this.handleSearchSubmit}>
+                        Search
+                      </FormBtn>
+                      <PoweredByGoogle />
+                    </form>
+                    <Card>
+                      <h1>Google Places search results</h1>
+                    </Card>
+                    {this.state.places.length ? (
+                      <List>
+                        {this.state.places.map(place => (
+                          <ListItem key={place.id}>
+                              <Card>
+                                <strong>
+                                    {place.name} at {place.formatted_address}
+                                <br />
+                                </strong>
+                                {place.photos[0].html_attributions[0]}
+                                {/* <a href={book.volumeInfo.infoLink}>View</a> */}
+                                <p>Rating: {place.rating}</p>
+                                <p>Price level: {place.price_level}</p>
+                                <br />
+                                {/* <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="Book" /> */}
+                              </Card>
+                              {/* <SaveBtn onClick={() => this.saveBook(book.volumeInfo)} /> */}
+                              <SaveBtn onClick={() => this.lovePlace(place)} />
+                              <DeleteBtn onClick={() => this.hatePlace(place)} />
+                          </ListItem>
+                        ))}
+                      </List>
+                    ) : (
+                    <h3>No Results to Display</h3>
+                    )}
+                  </Col>
+                </Row>
+              </Container>
+              </div>
+
+            <div style={{ opacity: this.state.signed ? 1 : 0 }}>
+            <Col size="md-12" >
+                <Container fluid>
                   <Card>
-                    <h1>Search Places</h1>
+                    <h1>Loved Places</h1>
                   </Card>
-                  {/* <Alert style={{ opacity: this.state.alert ? 1 : 0 }} type="success" >
-                  Book Saved
-                  <DeleteBtn onClick={() => this.setState({alert: false})} />
-                  </Alert> */}
-                  <form>
-                    <Input
-                      value={this.state.search}
-                      onChange={this.handleInputChange}
-                      name="search"
-                      placeholder="Search for"
-                    />
-                    <FormBtn disabled={!(this.state.search)} onClick={this.handleSearchSubmit}>
-                      Search
-                    </FormBtn>
-                    <PoweredByGoogle />
-                  </form>
-                  <Card>
-                    <h1>Google Places search results</h1>
-                  </Card>
-                  {this.state.places.length ? (
+                  {this.state.lovedplaces.length ? (
                     <List>
-                      {this.state.places.map(place => (
-                        <ListItem key={place.id}>
+                      {this.state.lovedplaces.map(place => (
+                        <ListItem key={place._id}>
+                          <Container>
                             <Card>
-                              <strong>
-                                  {place.name} at {place.formatted_address}
-                              <br />
-                              </strong>
-                              <img src ={ place.photos[0].html_attributions[0]}></img>
-                              {/* <a href={book.volumeInfo.infoLink}>View</a> */}
-                              <p>Rating: {place.rating}</p>
-                              <br />
-                              {/* <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="Book" /> */}
+                              {place.name}
                             </Card>
-                            {/* <SaveBtn onClick={() => this.saveBook(book.volumeInfo)} /> */}
+                            <Card>
+                              <List>
+                                {place.lovedcomment.map(comment =>(
+                                  <ListItem key={comment._id}>
+                                    <Card>
+                                      Comment: {comment.body}
+                                    </Card>
+                                  </ListItem>
+                                ))}
+                              </List>
+                            </Card>
+                            <DeleteBtn onClick={() => this.deletePlace(place._id)} />
+                            <form>
+                              <TextArea
+                                value={this.state.comment}
+                                onChange={this.handleInputChange}
+                                name="comment"
+                                placeholder="What is so good about it?"
+                              />
+                              <FormBtn
+                                disabled={!(this.state.comment)}
+                                id={place._id}
+                                onClick={this.handleLovedCommentSubmit}
+                              >
+                                Submit Comment
+                              </FormBtn>
+                            </form>
+  
+                          </Container>
                         </ListItem>
                       ))}
                     </List>
-                  ) : (
-                  <h3>No Results to Display</h3>
-                  )}
-                </Col>
-              </Row>
-            </Container>
-        </div>
-  );
-}
-}
+                  ): (
+                  <h3>No Results to Display</h3>)}
+                  </Container>  
+              </Col>
+              <Col size="md-12">
+                <Container fluid>
+                  <Card>
+                    <h1>Hated Places</h1>
+                  </Card>
+                  {this.state.hatedplaces.length ? (
+                    <List>
+                      {this.state.hatedplaces.map(place => (
+                        <ListItem key={place._id}>
+                          <div>
+                            <Card>
+                              {place.name}
+                            </Card>
+                            <Card>
+                              <List>
+                                {place.hatedcomment.map(comment =>(
+                                  <ListItem key={comment._id}>
+                                    <Card>
+                                      Comment: {comment.body}
+                                    </Card>
+                                  </ListItem>
+                                ))}
+                              </List>
+                            </Card>
+                            <DeleteBtn onClick={() => this.deletePlace(place._id)} />
+                            <form>
+                              <TextArea
+                                value={this.state.comment}
+                                onChange={this.handleInputChange}
+                                name="comment"
+                                placeholder="Why did you hate it?"
+                              />
+                              <FormBtn
+                                disabled={!(this.state.comment)}
+                                id={place._id}
+                                onClick={this.handleHatedCommentSubmit}
+                              >
+                                Submit Comment
+                              </FormBtn>
+                            </form>
+                          </div>
+                        </ListItem>
+                      ))}
+                    </List>
+                  ): (
+                  <h3>No Results to Display</h3>)}
+                  </Container> 
+              </Col>
+              </div>
+
+            </div>
+
+      );
+    }
+  }
+
 
 
 export default Dashboard;
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -40,19 +40,16 @@ class LogIn extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-
-    console.log("state: ", this.state)
-
-    this.props.history.push({
-          pathname: '/Dashboard',
-          state: this.state
-          }
-        
-    )
- 
+    if (this.state.username && this.state.password) {
+      API.findUser({
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(res => this.signIn(res))
+      .catch(err => console.log(err));
+    }
   };
 
-  
   signIn =(res)=>{
     if(res.data) {
       this.setState({
@@ -62,6 +59,11 @@ class LogIn extends Component {
         lovedplaces: res.data.swipedright,
         hatedplaces: res.data.swipedleft
       })
+      this.props.history.push({
+        pathname: '/Dashboard',
+        state: this.state
+        }
+  )
     }
     else{
       this.setState({
@@ -74,68 +76,19 @@ class LogIn extends Component {
     }
   }
 
-  handleSearchSubmit = event => {
-    event.preventDefault();
-    API.getPlacesGoogle(this.state.search)
-    .then(res=>{
-      this.setState({ places: res.data, search: ""})
-    })
-   .catch(err => console.log(err));
-  };
 
-  lovePlace = placeData => {
-    API.lovePlace(this.state.userID, placeData)
-    .then(res => {
-      this.setState({        
-        lovedplaces: res.data.swipedright
-      })
-    })
-    .catch(err => console.log(err));
-  };
 
-  hatePlace = placeData => {
-    API.hatePlace(this.state.userID, placeData)
-    .then(res => {
-      this.setState({
-        hatedplaces: res.data.swipedleft
-      })
-    })
-    .catch(err => console.log(err));
-  };
-
-  handleHatedCommentSubmit = event => {
-    event.preventDefault();
-    const id = event.target.id
-    API.postHatedComment(id, this.state.comment)
-    .then(res => {
-      console.log(res)
-      this.setState({     
-        comment: ""
-      })
-    })
-    .catch(err => console.log(err));
-  }
-
-  handleLovedCommentSubmit = event => {
-    const id = event.target.id
-    event.preventDefault();
-    API.postLovedComment(id, this.state.comment)
-    .then(res => {
-      this.setState({        
-        comment: ""
-      })
-    })
-    .catch(err => console.log(err));
-  }
 
   render() {
     return (
-      // Enter username and password
       <Container fluid>
-        <br /> <br /> <br /> <br /> <br /> <br /> <br />
+        <br /> 
+        <br />
+        <br />
+        <br />
         <Row>
           <Col size="md-6">
-            <form style={{ display: this.state.signed ? "none" : "block" }}>
+            <form style={{ opacity: this.state.signed ? 0 : 1 }}>
               <Input
                 value={this.state.username}
                 onChange={this.handleInputChange}
@@ -159,165 +112,8 @@ class LogIn extends Component {
               Log in failed. Check username and password.
               <DeleteBtn onClick={() => this.setState({alert: false})} />
             </Alert>
-            <div style={{ opacity: this.state.signed ? 1 : 0 }}>
-      {/* Page after Loggin In */}
-              <h1>Content for signed in users will go here</h1>
-              <MapG />
-              <Container fluid>
-              <Row>
-                <Col size="md-12">
-                  <Jumbotron>
-                    <h1>Search Places</h1>
-                  </Jumbotron>
-                  {/* <Alert style={{ opacity: this.state.alert ? 1 : 0 }} type="success" >
-                  Book Saved
-                  <DeleteBtn onClick={() => this.setState({alert: false})} />
-                  </Alert> */}
-                  <form>
-                    <Input
-                      value={this.state.search}
-                      onChange={this.handleInputChange}
-                      name="search"
-                      placeholder="Search for"
-                    />
-                    <FormBtn disabled={!(this.state.search)}onClick={this.handleSearchSubmit}>
-                      Search
-                    </FormBtn>
-                    <PoweredByGoogle />
-                  </form>
-                  <Jumbotron>
-                    <h1>Google Places search results</h1>
-                  </Jumbotron>
-                  {this.state.places.length ? (
-                    <List>
-                      {this.state.places.map(place => (
-                        <ListItem key={place.id}>
-                            <Jumbotron>
-                              <strong>
-                                  {place.name} at {place.formatted_address}
-                              <br />
-                              </strong>
-                              {place.photos[0].html_attributions[0]}
-                              {/* <a href={book.volumeInfo.infoLink}>View</a> */}
-                              <p>Rating: {place.rating}</p>
-                              <p>Price level: {place.price_level}</p>
-                              <br />
-                              {/* <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="Book" /> */}
-                            </Jumbotron>
-                            {/* <SaveBtn onClick={() => this.saveBook(book.volumeInfo)} /> */}
-                            <SaveBtn onClick={() => this.lovePlace(place)} />
-                            <DeleteBtn onClick={() => this.hatePlace(place)} />
-                        </ListItem>
-                      ))}
-                    </List>
-                  ) : (
-                  <h3>No Results to Display</h3>
-                  )}
-                </Col>
-              </Row>
-            </Container>
-            </div>
           </Col>
-          <div style={{ opacity: this.state.signed ? 1 : 0 }}>
-          <Col size="md-12" >
-              <Container fluid>
-                <Jumbotron>
-                  <h1>Loved Places</h1>
-                </Jumbotron>
-                {this.state.lovedplaces.length ? (
-                  <List>
-                    {this.state.lovedplaces.map(place => (
-                      <ListItem key={place._id}>
-                        <Container>
-                          <Jumbotron>
-                            {place.name}
-                          </Jumbotron>
-                          <Jumbotron>
-                            <List>
-                              {place.lovedcomment.map(comment =>(
-                                <ListItem key={comment._id}>
-                                  <Jumbotron>
-                                    Comment: {comment.body}
-                                  </Jumbotron>
-                                </ListItem>
-                              ))}
-                            </List>
-                          </Jumbotron>
-                          <DeleteBtn onClick={() => this.deletePlace(place._id)} />
-                          <form>
-                            <TextArea
-                              value={this.state.comment}
-                              onChange={this.handleInputChange}
-                              name="comment"
-                              placeholder="What is so good about it?"
-                            />
-                            <FormBtn
-                              disabled={!(this.state.comment)}
-                              id={place._id}
-                              onClick={this.handleLovedCommentSubmit}
-                            >
-                              Submit Comment
-                            </FormBtn>
-                          </form>
-
-                        </Container>
-                      </ListItem>
-                    ))}
-                  </List>
-                ): (
-                <h3>No Results to Display</h3>)}
-                </Container>  
-            </Col>
-            <Col size="md-12">
-              <Container fluid>
-                <Jumbotron>
-                  <h1>Hated Places</h1>
-                </Jumbotron>
-                {this.state.hatedplaces.length ? (
-                  <List>
-                    {this.state.hatedplaces.map(place => (
-                      <ListItem key={place._id}>
-                        <div>
-                          <Jumbotron>
-                            {place.name}
-                          </Jumbotron>
-                          <Jumbotron>
-                            <List>
-                              {place.hatedcomment.map(comment =>(
-                                <ListItem key={comment._id}>
-                                  <Jumbotron>
-                                    Comment: {comment.body}
-                                  </Jumbotron>
-                                </ListItem>
-                              ))}
-                            </List>
-                          </Jumbotron>
-                          <DeleteBtn onClick={() => this.deletePlace(place._id)} />
-                          <form>
-                            <TextArea
-                              value={this.state.comment}
-                              onChange={this.handleInputChange}
-                              name="comment"
-                              placeholder="Why did you hate it?"
-                            />
-                            <FormBtn
-                              disabled={!(this.state.comment)}
-                              id={place._id}
-                              onClick={this.handleHatedCommentSubmit}
-                            >
-                              Submit Comment
-                            </FormBtn>
-                          </form>
-                        </div>
-                      </ListItem>
-                    ))}
-                  </List>
-                ): (
-                <h3>No Results to Display</h3>)}
-                </Container> 
-            </Col>
-            </div>
-          </Row>
+        </Row>
       </Container>
     );
   }
